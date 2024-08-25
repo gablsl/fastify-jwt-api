99d06010df9e28b31d0188976ccf92387114c7f9
gablsl <gabrielsousadeveloper@gmail.com>
2024-08-25 20:30:58 -0300

    feat: add sign in route

--- src/routes/user/userRoutes.ts
+++ src/routes/user/userRoutes.ts
@@ -4,6 +4,7 @@ import {
     getUserById,
     getUsers,
     putUser,
+    signIn,
     signUp,
 } from '../../controllers/user/userController';
 
@@ -13,6 +14,7 @@ export const userRoutes: FastifyPluginAsync = async (fastify) => {
     fastify.get('/', getUsers);
     fastify.get('/:id', getUserById as SignUpHandler);
     fastify.post('/signup', signUp as SignUpHandler);
+    fastify.post('/signin', signIn as SignUpHandler);
     fastify.put('/:id', putUser as SignUpHandler);
     fastify.delete('/:id', delUser as SignUpHandler);
 };
